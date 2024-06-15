0ae12c12c92d391ed7d6ed890e85dd99aaea0823
liuzhi19121999 <1679950040@qq.com>
2024-06-15 12:02:15 +0800

    train change

--- AnimeGAN.py
+++ AnimeGAN.py
@@ -7,11 +7,15 @@ from tools.ops import con_loss_fn, style_loss_decentralization_3, region_smoothi
 from tools.GuidedFilter import guided_filter
 from torch import optim, Tensor
 from torchvision.transforms.v2.functional import rgb_to_grayscale
-import numpy as np
+from torch.utils.data.dataloader import DataLoader
 import torch
+import numpy as np
 from skimage import segmentation, color
 from joblib import Parallel, delayed
+from dataSet import ImageDataSet
+from tools.utils import check_folder
 import cv2
+from time import time
 
 def grayscale_to_rgb(input_tensor: Tensor) -> Tensor:
     if input_tensor.shape.__len__() == 4:
@@ -19,61 +23,77 @@ def grayscale_to_rgb(input_tensor: Tensor) -> Tensor:
     return input_tensor.expand((3, -1, -1))
 
 class Trainer:
-    def __init__(self, dataset="Hayao", init_lr_g=2e-4, lr_g=1e-4, lr_d=1e-4, device="cpu"):
+    def __init__(self, dataset="SummerWar", epoch=100, start_epoch=0, data_set_num=800, batch=4, init_g_epoch=10, init_lr_g=2e-4, lr_g=1e-4, lr_d=1e-4, device="cpu"):
         super(Trainer, self).__init__()
+        self.epoch = epoch
+        self.data_set_num = data_set_num
+        self.batch = batch
+        self.init_g_epoch = init_g_epoch
         self.device_type = device
+        self.data_dir = dataset
+        self.start_epoch = start_epoch
+
         self.G = GeneratorV3(dataset=dataset)
         self.D = DiscrimeV3()
         self.optimizer_init_g = optim.Adam(self.G.parameters(), lr=init_lr_g, betas=(0.5, 0.999))
         self.optimizer_g = optim.Adam(self.G.parameters(), lr=lr_g, betas=(0.5, 0.999))
         self.optimizer_d = optim.Adam(self.D.parameters(), lr=lr_d, betas=(0.5, 0.999))
+        self.data_set = ImageDataSet(dataset)
+
+        self.load_model_data()
     
-    def train(self):
-        '''训练模型'''
-        photo = torch.rand((2, 3, 512, 512))
-        real_photo = torch.rand((2, 3, 512, 512))
-        photo_superpixel = torch.rand((2, 3, 512, 512))
-
-        anime = torch.rand((2, 3, 512, 512))
-        anime_smooth = torch.rand((2, 3, 512, 512))
-
-        fake_superpixel = torch.rand((2, 3, 512, 512))
-        fake_NLMean_l0 = torch.rand((2, 3, 512, 512))
-        # 一次迭代
-        # generated and discrimination
+    def save_model_data(self):
+        '''保存模型数据'''
+        check_folder(f"./model_state/{self.data_dir}")
+        # G
+        torch.save({"model": self.G.state_dict()}, f"./model_state/{self.data_dir}/generator.pth")
+        # D
+        torch.save({"model": self.D.state_dict()}, f"./model_state/{self.data_dir}/discrime.pth")
+    
+    def load_model_data(self):
+        # G
+        try:
+            g_state = torch.load(f"./model_state/{self.data_dir}/generator.pth")
+            self.G.load_state_dict(g_state["model"])
+        except Exception:
+            pass
+        # D
+        try:
+            d_state = torch.load(f"./model_state/{self.data_dir}/discrime.pth")
+            self.D.load_state_dict(d_state["model"])
+        except Exception:
+            pass
+    
+    def init_g_train(self, photo: Tensor):
+        '''初始化生成器'''
+        self.optimizer_init_g.zero_grad()
         generated_s, generated_m = self.G(photo)
         generated = self.tanh_out_scale(guided_filter(self.sigm_out_scale(generated_s),self.sigm_out_scale(generated_s), 2, 0.01))
-        # val
-        val_generated_s, val_generated_m = self.G(real_photo)
-        val_generated = self.tanh_out_scale(guided_filter(self.sigm_out_scale(val_generated_s),self.sigm_out_scale(val_generated_s), 2, 0.01))
-        # gray maping
+        Pre_train_G_loss: Tensor = con_loss_fn(photo, generated)
+        Pre_train_G_loss.backward()
+        self.optimizer_init_g.step()
+        return Pre_train_G_loss.detach()
+    
+    def update_g(self, photo: Tensor, photo_superpixel: Tensor, anime: Tensor, anime_smooth: Tensor):
+        '''训练模型'''
+        # Update G Net
+        generated_s, generated_m = self.G(photo)
+        generated = self.tanh_out_scale(guided_filter(self.sigm_out_scale(generated_s),self.sigm_out_scale(generated_s), 2, 0.01))
+        fake_superpixel = self.get_seg(generated)
+        fake_NLMean_l0 = self.get_NLMean_l0(generated_s)
         fake_sty_gray = grayscale_to_rgb(rgb_to_grayscale(generated))
         anime_sty_gray = grayscale_to_rgb(rgb_to_grayscale(anime))
         gray_anime_smooth = grayscale_to_rgb(rgb_to_grayscale(anime_smooth))
-        # support
-        fake_gray_logit = self.D(fake_sty_gray)
-        anime_gray_logit = self.D(anime_sty_gray)
-        gray_anime_smooth_logit = self.D(gray_anime_smooth)
-        # main
-        generated_m_logit = self.D(generated_m)
-        fake_NLMean_logit = self.D(fake_NLMean_l0)
-        # loss
-        Pre_train_G_loss: Tensor = con_loss_fn(real_photo, generated)
         # GAN Support
-        con_loss =  con_loss_fn(real_photo, generated, 0.5)
-        s22, s33, s44  = style_loss_decentralization_3(anime_sty_gray, fake_sty_gray,  [0.1, 2.0,  28]) 
+        con_loss =  con_loss_fn(photo, generated, 0.5)
+        s22, s33, s44  = style_loss_decentralization_3(anime_sty_gray, fake_sty_gray,  [0.1, 2.0,  28])
         sty_loss = s22 + s33 + s44
-
         rs_loss =  region_smoothing_loss(fake_superpixel, generated, 0.8 ) + \
                         VGG_LOSS(photo_superpixel, generated) * 0.5
-
-        color_loss =  Lab_color_loss(real_photo, generated, 8. )
+        color_loss =  Lab_color_loss(photo, generated, 8. )
         tv_loss  = 0.0001 * total_variation_loss(generated)
-
         g_adv_loss = generator_loss(fake_gray_logit)
         G_support_loss = g_adv_loss + con_loss + sty_loss + rs_loss + color_loss + tv_loss
-        D_support_loss = discriminator_loss(anime_gray_logit, fake_gray_logit) \
-                            + discriminator_loss_346(gray_anime_smooth_logit) * 5.
         # main
         tv_loss_m = 0.0001 * total_variation_loss(generated_m)
         p4_loss = VGG_LOSS(fake_NLMean_l0, generated_m) * 0.5
@@ -81,10 +101,98 @@ class Trainer:
         g_m_loss = generator_loss_m(generated_m_logit) * 0.02
 
         G_main_loss = g_m_loss + p0_loss + p4_loss + tv_loss_m
-        D_main_loss = discriminator_loss_m(fake_NLMean_logit, generated_m_logit) * 0.1
-
         Generator_loss: Tensor =  G_support_loss +  G_main_loss
+        self.optimizer_g.zero_grad()
+        Generator_loss.backward()
+        self.optimizer_g.step()
+        
+        # support
+        fake_gray_logit = self.D(fake_sty_gray)
+        anime_gray_logit = self.D(anime_sty_gray)
+        gray_anime_smooth_logit = self.D(gray_anime_smooth)
+        # main
+        generated_m_logit = self.D(generated_m)
+        fake_NLMean_logit = self.D(fake_NLMean_l0)
+        D_support_loss = discriminator_loss(anime_gray_logit, fake_gray_logit) \
+                            + discriminator_loss_346(gray_anime_smooth_logit) * 5.
+        D_main_loss = discriminator_loss_m(fake_NLMean_logit, generated_m_logit) * 0.1
         Discriminator_loss: Tensor = D_support_loss + D_main_loss
+        self.optimizer_d.zero_grad()
+        Discriminator_loss.backward()
+        self.optimizer_d.step()
+
+        return Generator_loss.detach(), Discriminator_loss.detach()
+    
+    def train(self):
+        '''训练模型'''
+        for epo in range(self.start_epoch, self.epoch + 1):
+            # Steps of Every Epoch
+            data_loader = DataLoader(self.data_set, batch_size=self.batch, shuffle=False)
+            for (step, data_batch) in enumerate(data_loader):
+                start_time = time()
+                real_photo = torch.concat(data_batch["photo"])
+                anime = torch.concat(data_batch["style"])
+                anime_smooth = torch.concat(data_batch["smooth"])
+                
+                if epo < self.init_g_epoch:
+                    self.init_g_train(real_photo)
+
+
+        # photo = torch.rand((2, 3, 512, 512))
+        # real_photo = torch.rand((2, 3, 512, 512))
+        # photo_superpixel = torch.rand((2, 3, 512, 512))
+
+        # anime = torch.rand((2, 3, 512, 512))
+        # anime_smooth = torch.rand((2, 3, 512, 512))
+
+        # fake_superpixel = torch.rand((2, 3, 512, 512))
+        # fake_NLMean_l0 = torch.rand((2, 3, 512, 512))
+        # # 一次迭代
+        # # generated and discrimination
+        # generated_s, generated_m = self.G(photo)
+        # generated = self.tanh_out_scale(guided_filter(self.sigm_out_scale(generated_s),self.sigm_out_scale(generated_s), 2, 0.01))
+        # # val
+        # val_generated_s, val_generated_m = self.G(real_photo)
+        # val_generated = self.tanh_out_scale(guided_filter(self.sigm_out_scale(val_generated_s),self.sigm_out_scale(val_generated_s), 2, 0.01))
+        # # gray maping
+        # fake_sty_gray = grayscale_to_rgb(rgb_to_grayscale(generated))
+        # anime_sty_gray = grayscale_to_rgb(rgb_to_grayscale(anime))
+        # gray_anime_smooth = grayscale_to_rgb(rgb_to_grayscale(anime_smooth))
+        # # support
+        # fake_gray_logit = self.D(fake_sty_gray)
+        # anime_gray_logit = self.D(anime_sty_gray)
+        # gray_anime_smooth_logit = self.D(gray_anime_smooth)
+        # # main
+        # generated_m_logit = self.D(generated_m)
+        # fake_NLMean_logit = self.D(fake_NLMean_l0)
+        # # loss
+        # Pre_train_G_loss: Tensor = con_loss_fn(real_photo, generated)
+        # # GAN Support
+        # con_loss =  con_loss_fn(real_photo, generated, 0.5)
+        # s22, s33, s44  = style_loss_decentralization_3(anime_sty_gray, fake_sty_gray,  [0.1, 2.0,  28]) 
+        # sty_loss = s22 + s33 + s44
+
+        # rs_loss =  region_smoothing_loss(fake_superpixel, generated, 0.8 ) + \
+        #                 VGG_LOSS(photo_superpixel, generated) * 0.5
+
+        # color_loss =  Lab_color_loss(real_photo, generated, 8. )
+        # tv_loss  = 0.0001 * total_variation_loss(generated)
+
+        # g_adv_loss = generator_loss(fake_gray_logit)
+        # G_support_loss = g_adv_loss + con_loss + sty_loss + rs_loss + color_loss + tv_loss
+        # D_support_loss = discriminator_loss(anime_gray_logit, fake_gray_logit) \
+        #                     + discriminator_loss_346(gray_anime_smooth_logit) * 5.
+        # # main
+        # tv_loss_m = 0.0001 * total_variation_loss(generated_m)
+        # p4_loss = VGG_LOSS(fake_NLMean_l0, generated_m) * 0.5
+        # p0_loss = L1_loss(fake_NLMean_l0, generated_m) * 50
+        # g_m_loss = generator_loss_m(generated_m_logit) * 0.02
+
+        # G_main_loss = g_m_loss + p0_loss + p4_loss + tv_loss_m
+        # D_main_loss = discriminator_loss_m(fake_NLMean_logit, generated_m_logit) * 0.1
+
+        # Generator_loss: Tensor =  G_support_loss +  G_main_loss
+        # Discriminator_loss: Tensor = D_support_loss + D_main_loss
     
     def get_seg(self, batch_image):
         def get_superpixel(image):
@@ -145,3 +253,7 @@ class Trainer:
         # [0.0, 1.0]   to  [-1.0, 1.0]
         x = (x - 0.5) * 2.0
         return  torch.clamp(x,-1.0, 1.0)
+
+if __name__ == "__main__":
+    train = Trainer()
+    train.train()
